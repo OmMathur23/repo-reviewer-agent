7b9fda1db0713677aeeadc5314c17ae289607b13
Om Mathur <ommathur2303@gmail.com>
2026-07-30 23:58:01 +0530

    Updated tools/__init__.py

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,7 +1,11 @@
 from tools.file_reader import read_file
 from tools.clone_repo import clone_repo
+from tools.list_directory import list_directory
+from tools.search import search_code
 
 TOOLS = {
     "read_file": read_file,
     "clone_repo" : clone_repo,
+    "list_directory" : list_directory,
+    "search_code" : search_code,
 }
